344560d03be1e4eec8f876a512e209ffb11ff4bc
vitasika <vitasika1@gmail.com>
2022-04-04 22:43:53 +0300

    npm install simplelightbox

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './sass/main.scss';
+// all modules
+import Notiflix from 'notiflix';
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
