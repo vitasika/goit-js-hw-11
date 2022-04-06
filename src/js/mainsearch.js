c2e6d765018e90d1bcd700e3ba7d02a798a1d08b
vitasika <vitasika1@gmail.com>
2022-04-06 18:52:27 +0300

    re-submit

--- src/js/mainsearch.js
+++ src/js/mainsearch.js
@@ -26,26 +26,29 @@ async function onClickSubmit(e) {
     e.preventDefault();
     clearHitsContainer();
     
-  galleryApiService.query = e.currentTarget.elements.searchQuery.value.trim();
-  if (galleryApiService.query === '') {
+    galleryApiService.query = e.currentTarget.elements.searchQuery.value.trim();
+
+  if (galleryApiService.query === '') {    
       Notiflix.Notify.info('Please enter something');
-      
+      refs.loadMoreEl.classList.add('is-hidden');
       return;
     }
     
     galleryApiService.resetPage();
     
   try {
-      const urlObj = await galleryApiService.fetchGallery();
-
-      refs.loadMoreEl.classList.remove('is-hidden'); // скрыть кнопку при пустом поиске
     
-      const { data } = urlObj;
-      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images`);
-      
-      appendHitsMarkup(data.hits);
-      
-      lightbox.refresh(); //вызов лайтбокса
+    const urlObj = await galleryApiService.fetchGallery();
+    refs.loadMoreEl.classList.remove('is-hidden'); // скрыть кнопку при пустом поиске
+    
+    const { data } = urlObj;
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images`);      
+      appendHitsMarkup(data.hits);      
+    lightbox.refresh(); //вызов лайтбокса
+    
+    if (data.totalHits < galleryApiService.per_page) {
+      refs.loadMoreEl.classList.add('is-hidden');
+    }
       
   } catch (error) {
     Notiflix.Notify.failure(
@@ -56,30 +59,27 @@ async function onClickSubmit(e) {
 
 async function onLoadMore() {
     try {
-        const urlObj = await galleryApiService.fetchGallery();
-
-    const {
-      data: { hits },
-      } = urlObj;
+      const urlObj = await galleryApiService.fetchGallery();
       
-    appendHitsMarkup(hits);
-    lightbox.refresh();
-    const { height: cardHeight } = refs.divGallery.firstElementChild.getBoundingClientRect();
-    
+      const { data: { hits }, } = urlObj;
+      refs.loadMoreEl.classList.remove('is-hidden'); // возвращает кнопку
+      appendHitsMarkup(hits);
+      lightbox.refresh();
+      
+      const { height: cardHeight } = refs.divGallery.firstElementChild.getBoundingClientRect();    
       window.scrollBy({
       top: cardHeight * 2,
       behavior: 'smooth',
-    });
-
-    if (galleryApiService.page > urlObj.data.totalHits / galleryApiService.per_page) {
+      });
+      
+      if (galleryApiService.page > urlObj.data.totalHits / galleryApiService.per_page) {
       refs.loadMoreEl.classList.add('is-hidden');
       return Notiflix.Notify.success('Your search has come to an end');
-    }
-    } catch (error) {
+      }
 
-        refs.loadMoreEl.classList.add('is-hidden'); // скрыть кнопку при пустом поиске
-
-    Notiflix.Notify.failure(
+    } catch (error) {      
+      //refs.loadMoreEl.classList.add('is-hidden'); // добавить кнопку при пустом поиске
+      Notiflix.Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.',
     );
   }
